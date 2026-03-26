a4bd035a2d589cb540a3b25dba7f3129f5fc038a
Aditya-Goenka0207 <aditya.goenka0207@gmail.com>
2026-03-26 21:10:37 +0530

    Updated backend url

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:9090";
+export const BASE_URL = "https://linkedin-backend-4q4s.onrender.com";
 
 //separate axios instance for user routes
 export const userServer = axios.create({
